2c07ce035d8dcf8fb5a284f2fc60ea726aa8acd4
hollop90 <nozolo96@gmail.com>
2023-10-31 14:13:14 +0000

    everything

--- src/mydevice.c
+++ src/mydevice.c
@@ -5,8 +5,26 @@
 #include <zephyr/drivers/i2c.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 #include "mydevice.h"
+
+int16_t AC1;
+int16_t AC2;
+int16_t AC3;
+uint16_t AC4;
+uint16_t AC5;
+uint16_t AC6;
+int16_t B1;
+int16_t B2;
+int16_t MB;
+int16_t MC;
+int16_t MD;
+
+int32_t X1;
+int32_t X2;
+int32_t B5;
+
 static const struct device *i2c;
 int device_begin()
 {
@@ -36,3 +54,87 @@ int mydevice_writeRegister(uint8_t RegNum, uint8_t Value)
 	nack=i2c_reg_write_byte(i2c,DEVICE_ADDRESS,RegNum,Value);
     return nack;
 }
+
+// int read_uint(uint8_t addr, uint16_t *value){
+// 	//k_msleep(10);
+// 	uint8_t MSB;
+// 	uint8_t LSB;
+// 
+// 	if(mydevice_readRegister(addr, &MSB)){
+// 		printf("ERROR: Failed to read uint16_t from register\n");
+// 		return -1;
+// 	}
+// 	//printf("MSB: %x\n", MSB);
+// 	addr += 1;
+// 	k_msleep(10);
+// 	if(mydevice_readRegister(addr, &LSB)){
+// 		printf("ERROR: Failed to read uint16_t from register\n");
+// 		return -1;
+// 	}
+// 	//printf("LSB: %x\n", LSB);
+// 
+// 	*value = (MSB<<8)|(LSB);
+// 	return 0;
+// }
+// 
+// int read_int(uint8_t addr, int16_t *value){
+// 	k_msleep(10);
+// 	uint8_t MSB;
+// 	uint8_t LSB;
+// 
+// 	if(mydevice_readRegister(addr, &MSB)){
+// 		printf("ERROR: Failed to read int16_t from register\n");
+// 		return -1;
+// 	}
+// 	//printf("MSB: %x\n", MSB);
+// 	addr += 1;
+// 	k_msleep(10);
+// 	if(mydevice_readRegister(addr, &LSB)){
+// 		printf("ERROR: Failed to read int16_t from register\n");
+// 		return -1;
+// 	}
+// 	//printf("LSB: %x\n", LSB);
+// 
+// 	*value = MSB<<8;
+// 	*value = *value|LSB;
+// 	return 0;
+// }
+
+float get_temp(){
+	uint8_t retval = 0;
+	int16_t temp = 0;
+	float true_temp;
+
+	mydevice_readRegister(0x41, &retval);
+	temp = retval<<8;
+	mydevice_readRegister(0x42, &retval);
+	temp |= retval;
+
+	true_temp = (float)temp/340 + 36.53;
+	return true_temp;
+}
+
+int get_z_accel_raw(){
+	uint8_t retval = 0;
+	int16_t accel = 0;
+	float true_accel;
+
+	mydevice_readRegister(0x3f, &retval);
+	accel = retval<<8;
+	mydevice_readRegister(0x40, &retval);
+	accel |= retval;
+
+	return accel;
+}
+
+float get_z_accel(){
+	uint8_t retval = 0;
+	int16_t accel = 0;
+	float true_accel;
+
+	accel = get_z_accel_raw();
+
+	true_accel = (float)(accel/16384);
+	return true_accel;
+}
+
--- src/mydevice.h
+++ src/mydevice.h
@@ -1,9 +1,44 @@
+#ifndef UGO_LIB
+#define UGO_LIB
+
 #include <stdio.h>
 #include <stdint.h>
-#define DEVICE_ADDRESS (0x77)
+#define DEVICE_ADDRESS (0x68) //mpu6050
+//#define DEVICE_ADDRESS (0x52) // rv3028
 
 int device_begin();
 
 int mydevice_readRegister(uint8_t RegNum, uint8_t *Value);
 
 int mydevice_writeRegister(uint8_t RegNum, uint8_t Value);
+
+// Read the calibration values
+extern int16_t AC1;
+extern int16_t AC2;
+extern int16_t AC3;
+extern uint16_t AC4;
+extern uint16_t AC5;
+extern uint16_t AC6;
+extern int16_t B1;
+extern int16_t B2;
+extern int16_t MB;
+extern int16_t MC;
+extern int16_t MD;
+
+extern int32_t X1;
+extern int32_t X2;
+extern int32_t B5;
+
+int read_int(uint8_t addr, int16_t *value);
+int read_uint(uint8_t addr, uint16_t *value);
+
+int fetch_cali_vals();
+
+uint16_t get_raw_temp();
+float get_temp();
+
+void calc_constants(uint16_t UT);
+
+int get_z_accel_raw();
+float get_z_accel();
+#endif
