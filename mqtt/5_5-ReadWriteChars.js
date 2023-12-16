52c2decf7b5fbcddd123674129c4ad7e7bcccbc7
hollop90 <nozolo96@gmail.com>
2023-12-16 16:31:45 +0000

    converted accelerometer reading to integer

--- mqtt/5_5-ReadWriteChars.js
+++ mqtt/5_5-ReadWriteChars.js
@@ -7,16 +7,18 @@ const accelService = "00000001-0002-0003-0004-000000000000"
 const accelCharX = "00000001-0002-0003-0004-000000000001"
 const accelCharY = "00000001-0002-0003-0004-000000000002"
 const accelCharZ = "00000001-0002-0003-0004-000000000003"
+
 const main = async () => {
+  const { Buffer } = require('node:buffer')
   const { createBluetooth } = require('node-ble')
-  const { bluetooth, destroy } = createBluetooth() //this is the same as const bluetooth = (object returned by createBluetooth()).bluetooth; const destroy = (object returned by createBluetooth()).destroy; 
+  const { bluetooth, destroy } = createBluetooth() //this is the same as const bluetooth = (object returned by createBluetooth()).bluetooth; const destroy = (object returned by createBluetooth()).destroy;
 
   // get bluetooth adapter
   const adapter = await bluetooth.defaultAdapter() //get an available Bluetooth adapter
-  await adapter.startDiscovery() //using the adapter, start a device discovery session  
+  await adapter.startDiscovery() //using the adapter, start a device discovery session
   console.log('discovering')
 
-  // look for a specific device 
+  // look for a specific device
   const device = await adapter.waitDevice(deviceOfInterest)
   console.log('got device', await device.getAddress())
   const deviceName = await device.getName()
@@ -30,23 +32,29 @@ const main = async () => {
   services = await gattServer.services()
   console.log("services are " + services)
 
-  if (services.includes(serviceOfInterestUuid)) { //uuid of service
+  if (services.includes(accelService)) { //uuid of service
     console.log('got the LED service')
-    const primaryService = await gattServer.getPrimaryService(serviceOfInterestUuid)
+    const primaryService = await gattServer.getPrimaryService(accelService)
+    const ledService = await gattServer.getPrimaryService(serviceOfInterestUuid)
     chars = await primaryService.characteristics()
     console.log("the service characteristics are : " + chars)
 
-    console.log("uuid of characteristic of interest is : " + characteristicOfInterestUuid)
-    charact = await primaryService.getCharacteristic(characteristicOfInterestUuid)
+    console.log("uuid of accel_x is : " + accelCharZ)
+    charact = await primaryService.getCharacteristic(accelCharZ)
+    ledChar = await ledService.getCharacteristic(characteristicOfInterestUuid)
     console.log("characteristic flags are : " + await charact.getFlags())
     cval = await charact.readValue()
-    console.log("characteristic value is : " + (await charact.readValue()).toString())
-    await charact.writeValue(new Buffer.from([1])) //write to LED characteristic to turn LED on
+    buf = Buffer.from(cval)
+    console.log(buf.readInt32LE(0)) // Accel val is a 32bit integer sent in LE ranging from -1024 to +1024
+    console.log("characteristic value is : " + (await charact.readValue()))
+    console.log("characteristic value is : " + (await charact.readValue()))
+    console.log("characteristic value is : " + (await charact.readValue()))
+    console.log("characteristic value is : " + (await charact.readValue()))
 
+    await ledChar.writeValue(new Buffer.from([1])) //write to LED characteristic to turn LED on
     console.log("LED is on")
-    await new Promise(resolve => setTimeout(resolve, 2000));//delay for 2 seconds
-    await charact.writeValue(new Buffer.from([0])) //write to LED characteristic to turn LED on
-
+    await new Promise(resolve => setTimeout(resolve, 200));//delay for 2 seconds
+    await ledChar.writeValue(new Buffer.from([0])) //write to LED characteristic to turn LED on
     console.log("LED is off")
   }
 
@@ -58,3 +66,4 @@ const main = async () => {
 main()
   .then()
   .catch(console.error)
+
